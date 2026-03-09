98cd4bda1d5e408276f9731d7f9ebfe893dd3edc
Akshai N V <akshaiachu012@gmail.com>
2026-03-09 17:05:58 +0530

    footer change

--- src/app/core/footer/footer.ts
+++ src/app/core/footer/footer.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { AppointmentService } from '../../services/appointment.service';
 
 @Component({
   selector: 'app-footer',
+  standalone: true,
   imports: [RouterLink],
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
 export class Footer {
+  constructor(private appointmentService: AppointmentService) { }
 
+  openBooking() {
+    this.appointmentService.triggerBookingModal();
+  }
 }
